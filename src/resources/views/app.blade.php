3a5988a9a959bb9414b74294642e8a98a39b2136
zigzagdev <msts.oo0131@gmail.com>
2022-03-20 23:50:24 +0900

    title_change

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'heritage') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -20,8 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        fuck you
-    </div>
+  <div id="app">
+  </div>
 </body>
 </html>
